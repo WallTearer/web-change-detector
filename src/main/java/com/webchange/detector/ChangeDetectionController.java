1e4e2ce9609f9ca1988afbe682cac5b6c4f58117
WallTearer <walltearer@gmail.com>
2018-01-21 18:25:24 +0100

    Initial project setup

---
+++ src/main/java/com/webchange/detector/ChangeDetectionController.java
@@ -0,0 +1,14 @@
+package com.webchange.detector;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class ChangeDetectionController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Web Change Detector!";
+    }
+
+}
